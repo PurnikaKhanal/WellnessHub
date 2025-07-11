5b3aaa59145248df0d331677c8e8646218d9e42c
prijesshh <prijeshpradhan@gmail.com>
2025-07-11 17:31:41 +0545

    Initial commit

--- projectc++.cpp
+++ projectc++.cpp
@@ -1,17 +1,20 @@
     #include<iostream>
     #include<string>
+    #include<fstream>
     using namespace std;
     class pin
     {
         private:
         string initialpin;
-        string pn;
+        string newpin;
         int attempts=3;
         public:
+        //initial pin lai intialize
         pin()
         {
             initialpin="open";
         }
+        //check initialpin
         void check()
         { 
             while(attempts>0)
@@ -33,17 +36,62 @@
                 cout<<attempts<<" atempts are left"<<endl;
             }
         }
+        //change pin
         void authentication()
-        {
+        { 
             attempts=3;
-        
-        cout<<"enter your new password:"<<endl;
-        cin>>pn;
-        initialpin=pn;
-            while(attempts>0)
+        while(attempts>0)
+            {
+                string temp;
+                cout<<"enter your old password:"<<endl;
+                cin>>temp;
+                if(temp==initialpin && attempts!=0)
+                {
+                   cout<<"enter your new password"<<endl;
+                   cin>>newpin;
+           
+            ofstream file("pin.txt");  // overwrite in file
+            if (file.is_open()) {
+                file << newpin << endl;
+                file.close();
+                cout << "Password changed and saved successfully\n";
+            } else {
+                cout << "Failed to save password \n";
+            }
+
+                newcheck(); //new password check garna
+                break;
+            }
+                
+                else if(attempts==0)
+                {
+                    cout<<"too many incorect attempts \n acess denied.......";
+                    exit(0);
+                }
+                attempts--;
+                cout<<attempts<<" atempts are left"<<endl;
+            }
+        }
+        //check your new password
+    void newcheck()
+    { 
+         attempts=3;
+        string temp;
+         string checkpin;//file ma vako pass checkpin ma rakhne
+    ifstream file("pin.txt");
+
+    if (file.is_open()) {
+        getline(file, checkpin);  // read the first line into checkpin
+        file.close();
+    } else {
+        cout << "Unknown error occured\n";
+    }
+         while(attempts>0)
             {
                 attempts--;
-                if(pn==initialpin && attempts!=0)
+                cout<<"enter your password:"<<endl;
+                cin>>temp;
+                if(checkpin==temp && attempts!=0)
                 {
                 cout<<"acess granted"<<endl;
                 break;
@@ -56,14 +104,27 @@
                 }
             }
         }
-   
- 
+    
     };
     int main()
     {
+        string extract;//to check if file is empty
         int choice;
+          ifstream file("pin.txt");
         pin p;
+        if (file.is_open()) 
+    {
+        getline(file, extract);  // read first line from file into a
+        file.close();
+    }
+    
+        if (extract.empty()) 
+        {
         p.check();
+        }
+        else{
+            p.newcheck();
+        }
         do
         {
             cout<<"1.change password"<<endl;
